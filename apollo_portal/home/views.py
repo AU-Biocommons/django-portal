38b1e127f59e026ef1e3c6715579652e01b2e040
Cameron Hyde <chyde@neoformit.com>
2023-08-22 12:22:00 +1000

    FAQ page

--- apollo_portal/home/views.py
+++ apollo_portal/home/views.py
@@ -1,5 +1,47 @@
 from django.shortcuts import render
 
+RESOURCES_PAGES = [
+    {
+        'url': '/resources/start',
+        'label': 'Getting Started',
+    },
+    {
+        'url': '/resources/documentation',
+        'label': 'User Documentation',
+    },
+    {
+        'url': '/resources/training',
+        'label': 'Training, Tutorials and Other Resources',
+    },
+    {
+        'url': '/resources/faqs',
+        'label': 'FAQs',
+    },
+    {
+        'url': '/resources/video',
+        'label': 'Video Resources',
+    },
+    {
+        'url': '/resources/terms',
+        'label': 'Terms of Use',
+    },
+]
+
+
+def get_resource_nav_context(request):
+    """Return a context dict for prev and next pages."""
+    for i, item in enumerate(RESOURCES_PAGES):
+        if item['url'] == request.path:
+            break
+    return {
+        'prev': RESOURCES_PAGES[i-1] if i > 0 else None,
+        'next': (
+            RESOURCES_PAGES[i+1]
+            if i < len(RESOURCES_PAGES) - 1
+            else None
+        ),
+    }
+
 
 def index(request):
     return render(request, 'home/index.html')
@@ -16,27 +58,38 @@ def contact(request):
 
 
 def getting_started(request):
-    return render(request, 'home/resources/getting-started.html')
+    return render(request, 'home/resources/getting-started.html', {
+        'nav': get_resource_nav_context(request),
+    })
 
 
 def documentation(request):
-    return render(request, 'home/resources/documentation.html')
+    return render(request, 'home/resources/documentation.html', {
+        'nav': get_resource_nav_context(request),
+    })
 
 
 def training(request):
-    # TODO
-    return render(request, 'home/resources/training.html')
+    return render(request, 'home/resources/training.html', {
+        'nav': get_resource_nav_context(request),
+    })
 
 
 def faqs(request):
     # TODO
-    return render(request, 'home/resources/faqs.html')
+    return render(request, 'home/resources/faqs.html', {
+        'nav': get_resource_nav_context(request),
+    })
 
 
 def video(request):
     # TODO
-    return render(request, 'home/resources/video.html')
+    return render(request, 'home/resources/video.html', {
+        'nav': get_resource_nav_context(request),
+    })
 
 
 def terms(request):
-    return render(request, 'home/resources/terms.html')
+    return render(request, 'home/resources/terms.html', {
+        'nav': get_resource_nav_context(request),
+    })
