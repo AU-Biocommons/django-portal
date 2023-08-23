351f3a33e5981d5bddf741087d0fa965105f06c6
Cameron Hyde <chyde@neoformit.com>
2023-08-23 21:00:53 +1000

    Debugged OtherSelect widget

--- apollo_portal/home/widgets.py
+++ apollo_portal/home/widgets.py
@@ -12,10 +12,22 @@ class OtherSelect(Select):
 
     def get_context(self, name, value, attrs):
         context = super().get_context(name, value, attrs)
-        context['widget']['other_value'] = (
-            value
-            if value not in dict(self.choices)
-            else ''
-        )
+        if value not in dict(self.choices):
+            # Set the other field to selected:
+            group_name, group_choices, group_index = (
+                context['widget']['optgroups'][-1]
+            )
+            context['widget']['optgroups'][-1] = (
+                group_name, [
+                    {
+                        **group_choices[0],
+                        'selected': True,
+                        'attrs': {'selected': True},
+                    }
+                ], group_index)
+
+            # Set the other_value to whatever value is set:
+            context['widget']['other_value'] = value
+
         context['widget']['other_attrs'] = self.other_attrs
         return context
