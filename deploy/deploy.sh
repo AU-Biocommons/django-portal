693bd9c8d22812a51d8334c14fe50c2e242e957f
Cameron Hyde <chyde@neoformit.com>
2023-08-28 09:00:18 +1000

    Fix deploy script/config

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,19 +1,24 @@
 # Deploy to ubuntu server
 
-# TODO: link /srv/sites/apollo_portal to REPO_DIR so .env matches up
+# ! TODO: recreate venv in $REPO_DIR
 
 set -e
 
 CERTBOT_EMAIL=c.hyde@qcif.edu.au
-RUN_USER=c.hyde
+BUILD_USER=root
+RUN_USER=www-data
 RUN_GROUP=www-data
 HOSTNAME=django-sandpit.genome.edu.au
 APP_DIRNAME=apollo_portal
-REPO_DIR=~/apollo_portal
+REPO_DIR=/srv/sites/apollo_portal
 APP_DIR=$REPO_DIR/$APP_DIRNAME
+VENV_DIR=$REPO_DIR/venv
+STATIC_ROOT=$APP_DIR/apollo_portal/static
 GITHUB_URL=https://github.com/AU-Biocommons/django-portal.git
 SQLITE_FILEPATH=$APP_DIR/db.sqlite3
+DJANGO_SETTINGS_MODULE=apollo_portal.settings.prod
 
+sudo su $BUILD_USER
 
 # Clone repo
 git clone $GITHUB_URL $REPO_DIR
@@ -21,17 +26,18 @@ git clone $GITHUB_URL $REPO_DIR
 # Create venv and install python deps
 sudo apt install -y python3-pip
 sudo python3 -m pip install virtualenv
-virtualenv $APP_DIR/venv
-source $APP_DIR/venv/bin/activate
+virtualenv $VENV_DIR
+source $VENV_DIR/bin/activate
 pip install -r $REPO_DIR/requirements.txt
+echo "export DJANGO_SETTINGS_MODULE=$DJANGO_SETTINGS_MODULE" >> $VENV_DIR/bin/activate
 
 # Setup site dir
 sudo mkdir /srv/sites
-sudo ln -s $APP_DIR /srv/sites/$APP_DIRNAME
+sudo ln -s $REPO_DIR /srv/sites/$APP_DIRNAME
 
 # Setup nginx
 sudo apt install -y nginx
-sudo ln -s $REPO_DIR/scripts/deploy/nginx.conf /etc/nginx/sites-available/$HOSTNAME
+sudo ln -s $REPO_DIR/deploy/nginx.conf /etc/nginx/sites-available/$HOSTNAME
 sudo ln -s /etc/nginx/sites-available/$HOSTNAME /etc/nginx/sites-enabled/$HOSTNAME
 sudo rm /etc/nginx/sites-enabled/default
 sudo service nginx restart
@@ -41,8 +47,8 @@ sudo apt install -y certbot python3-certbot-nginx
 sudo certbot --nginx -d $HOSTNAME --non-interactive --agree-tos --email $CERTBOT_EMAIL
 
 # Setup gunicorn
-sudo ln -s $REPO_DIR/scripts/deploy/apollo_portal.service /etc/systemd/system/apollo_portal.service
-sudo ln -s $REPO_DIR/scripts/deploy/apollo_portal.socket /etc/systemd/system/apollo_portal.socket
+sudo ln -s $REPO_DIR/deploy/apollo_portal.service /etc/systemd/system/apollo_portal.service
+sudo ln -s $REPO_DIR/deploy/apollo_portal.socket /etc/systemd/system/apollo_portal.socket
 sudo systemctl enable apollo_portal.service
 sudo systemctl enable apollo_portal.socket
 
@@ -57,6 +63,7 @@ python manage.py migrate
 python manage.py build_index
 python manage.py collectstatic --noinput
 sudo chown $RUN_USER:$RUN_GROUP $SQLITE_FILEPATH
+sudo chown -R $RUN_USER:$RUN_GROUP $STATIC_ROOT
 
 # Start application
 sudo systemctl restart apollo_portal.service
