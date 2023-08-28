d2e7b106dea5e88804925cd5b6e90a12f96d3a20
Cameron Hyde <chyde@neoformit.com>
2023-08-28 14:42:25 +1000

    Set build_index exit code on error

--- apollo_portal/home/management/commands/build_index.py
+++ apollo_portal/home/management/commands/build_index.py
@@ -1,3 +1,4 @@
+import sys
 from django.core.management.base import BaseCommand
 from home import search
 
@@ -14,3 +15,4 @@ class Command(BaseCommand):
         else:
             self.stdout.write(self.style.ERROR(
                 '\nNo pages were indexed.\n'))
+            sys.exit(1)
