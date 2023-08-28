5320e2d46fe419abdfe2a3852f1867d7ba5fd533
Cameron Hyde <chyde@neoformit.com>
2023-08-28 14:48:57 +1000

    Add localhost to prod ALLOWED_HOSTS

--- apollo_portal/apollo_portal/settings/prod.py
+++ apollo_portal/apollo_portal/settings/prod.py
@@ -7,6 +7,8 @@ from .base import *
 SECRET_KEY = os.environ['SECRET_KEY']
 DEBUG = False
 ALLOWED_HOSTS += [
+    '127.0.0.1',
+    'localhost',
     'apollo-portal.genome.edu.au',
     'django-sandpit.genome.edu.au',
 ]
