41a75eeddeed62adb0d381638eb5f9d992bd4cc0
Cameron Hyde <chyde@neoformit.com>
2023-09-22 15:00:47 +1000

    Update prod settings

--- apollo_portal/apollo_portal/settings/prod.py
+++ apollo_portal/apollo_portal/settings/prod.py
@@ -1,13 +1,12 @@
 """Production settings."""
 
 import os
-from .base import *
+from .base import *  # noqa
 
 
 SECRET_KEY = os.environ['SECRET_KEY']
 DEBUG = False
-ALLOWED_HOSTS = [
-    '127.0.0.1',
+ALLOWED_HOSTS += [  # noqa
     'apollo-portal.genome.edu.au',
     'django-sandpit.genome.edu.au',
 ]
