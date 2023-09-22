a6b93cdfc3714bbae9290a3789069dac913c9bdf
Cameron Hyde <chyde@neoformit.com>
2023-09-22 15:24:16 +1000

    Cache busting with manifest static files

--- apollo_portal/apollo_portal/settings/prod.py
+++ apollo_portal/apollo_portal/settings/prod.py
@@ -10,3 +10,7 @@ ALLOWED_HOSTS += [  # noqa
     'apollo-portal.genome.edu.au',
     'django-sandpit.genome.edu.au',
 ]
+
+# Use manifest to manage static file versions for cache busting:
+STATICFILES_STORAGE = ('django.contrib.staticfiles.storage'
+                       '.ManifestStaticFilesStorage')
