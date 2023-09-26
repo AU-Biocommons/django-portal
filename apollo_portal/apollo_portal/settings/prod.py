b6dabbbdce669b13dc6d3f78d6b988039f0281c8
Cameron Hyde <chyde@neoformit.com>
2023-09-27 09:42:57 +1000

    Add scheme to CSRF_TRUSTED_ORIGINS

--- apollo_portal/apollo_portal/settings/prod.py
+++ apollo_portal/apollo_portal/settings/prod.py
@@ -11,7 +11,10 @@ PRODUCTION_HOSTNAMES = [
     'django-sandpit.genome.edu.au',
 ]
 ALLOWED_HOSTS += PRODUCTION_HOSTNAMES  # noqa
-CSRF_TRUSTED_ORIGINS = PRODUCTION_HOSTNAMES
+CSRF_TRUSTED_ORIGINS = [
+    f"https://{hostname}"
+    for hostname in PRODUCTION_HOSTNAMES
+]
 
 # Use manifest to manage static file versions for cache busting:
 STATICFILES_STORAGE = ('django.contrib.staticfiles.storage'
