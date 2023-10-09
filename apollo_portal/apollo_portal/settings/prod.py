ec3251ac566fa3b9eeb9a400609f0e0c2d9025e4
Cameron Hyde <chyde@neoformit.com>
2023-10-09 16:19:54 +1000

    Prod postgresql database

--- apollo_portal/apollo_portal/settings/prod.py
+++ apollo_portal/apollo_portal/settings/prod.py
@@ -1,7 +1,7 @@
 """Production settings."""
 
 import os
-from .base import *  # noqa
+from .base import *  # noqa: F403
 
 
 SECRET_KEY = os.environ['SECRET_KEY']
@@ -21,3 +21,12 @@ CSRF_TRUSTED_ORIGINS = [
 # Use manifest to manage static file versions for cache busting:
 STATICFILES_STORAGE = ('django.contrib.staticfiles.storage'
                        '.ManifestStaticFilesStorage')
+
+DATABASES['default'] = {  # noqa: F405
+    'ENGINE': 'django.db.backends.postgresql',
+    'NAME': os.environ['DB_NAME'],
+    'USER': os.environ['DB_USER'],
+    'PASSWORD': os.environ['DB_PASSWORD'],
+    'HOST': '127.0.0.1',
+    'PORT': '5432',
+}
