a839c41e2e9a832bb536320f30e86b4294acd0b4
Cameron Hyde <chyde@neoformit.com>
2023-10-23 09:14:58 +1000

    Fix tests

--- apollo_portal/apollo_portal/settings/test.py
+++ apollo_portal/apollo_portal/settings/test.py
@@ -1,4 +1,7 @@
-"""Override settings for test environment."""
+"""Override settings for test environment.
+
+Env variables from .env.test will override those in .env.
+"""
 
 import os
 from dotenv import load_dotenv
@@ -10,9 +13,6 @@ env_file = BASE_DIR.parent / '.env.test'  # noqa: F405
 if env_file.exists():
     load_dotenv(env_file, override=True)
 
-TEST_PRODUCTION_MAIL_SERVER = is_truthy_string(
-    os.environ.get('TEST_PRODUCTION_MAIL_SERVER'))
-
 SILENCED_SYSTEM_CHECKS = ['captcha.recaptcha_test_key_error']
 
 DATABASES = {
@@ -22,20 +22,19 @@ DATABASES = {
     }
 }
 
-EMAIL_BACKEND = 'django.core.mail.backends.locmem.EmailBackend'
-if TEST_PRODUCTION_MAIL_SERVER:
-    EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-    EMAIL_HOST = os.environ.get('MAIL_HOSTNAME')
-    EMAIL_PORT = os.environ.get('MAIL_SMTP_PORT')
-    EMAIL_HOST_USER = os.environ.get('MAIL_SMTP_USERNAME')
-    EMAIL_HOST_PASSWORD = os.environ.get('MAIL_SMTP_PASSWORD')
-    EMAIL_USE_TLS = (
-        str(os.environ.get('MAIL_USE_TLS')).lower()
-        in ('1', 'true')
-    )
-    EMAIL_FROM_ADDRESS = os.environ.get('MAIL_FROM_ADDRESS')
-    EMAIL_TO_ADDRESS = os.environ.get('MAIL_TO_ADDRESS')
-    SERVER_EMAIL = os.environ.get('MAIL_FROM_ADDRESS')
+TEST_PRODUCTION_MAIL_SERVER = is_truthy_string(
+    os.environ.get('TEST_PRODUCTION_MAIL_SERVER'))
+EMAIL_HOST = os.environ.get('MAIL_HOSTNAME')
+EMAIL_PORT = os.environ.get('MAIL_SMTP_PORT')
+EMAIL_HOST_USER = os.environ.get('MAIL_SMTP_USERNAME')
+EMAIL_HOST_PASSWORD = os.environ.get('MAIL_SMTP_PASSWORD')
+EMAIL_USE_TLS = (
+    str(os.environ.get('MAIL_USE_TLS')).lower()
+    in ('1', 'true')
+)
+EMAIL_FROM_ADDRESS = os.environ.get('MAIL_FROM_ADDRESS')
+EMAIL_TO_ADDRESS = os.environ.get('MAIL_TO_ADDRESS')
+SERVER_EMAIL = os.environ.get('MAIL_FROM_ADDRESS')
 
 # Google's test keys always pass validation
 RECAPTCHA_PUBLIC_KEY = '6LeIxAcTAAAAAJcZVRqyHh71UMIEGNQ_MXjiZKhI'
