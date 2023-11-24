8bca0f1cf3906e815c831631a27d43a4811b7ce4
Cameron Hyde <chyde@neoformit.com>
2023-11-24 10:40:44 +1000

    Add apollo-portal-dev hostname

--- apollo_portal/apollo_portal/settings/prod.py
+++ apollo_portal/apollo_portal/settings/prod.py
@@ -9,8 +9,7 @@ DEBUG = False
 PRODUCTION_HOSTNAMES = [
     'genome.edu.au',
     'apollo-portal.genome.edu.au',
-    'django-sandpit.genome.edu.au',
-    'ubuntu22-test.genome.edu.au',
+    'apollo-portal-dev.genome.edu.au',
 ]
 ALLOWED_HOSTS += PRODUCTION_HOSTNAMES  # noqa
 CSRF_TRUSTED_ORIGINS = [
