1ef12732111609e14f0e2263bf954e1b2f1a56a1
Cameron Hyde <chyde@neoformit.com>
2023-12-13 08:41:19 +1000

    Tidy up genome/tracks admin

--- apollo_portal/tracks/admin.py
+++ apollo_portal/tracks/admin.py
@@ -1,7 +1,46 @@
 from django.contrib import admin
+from django.utils.html import format_html
 
 from .models import Genome, Lab, Track
 
-admin.site.register(Genome)
+
+class GenomeAdmin(admin.ModelAdmin):
+    list_display = [
+        "name", "group", "_lab_name", "species", "strain", "datetime_created"]
+
+    @admin.display(ordering="genome__name")
+    def _genome_name(self, track):
+        return track.genome.name
+
+    @admin.display(ordering="lab__name")
+    def _lab_name(self, genome):
+        return format_html(
+            '<a href="/admin/tracks/lab/{}/">{}</a>',
+            genome.lab.id,
+            genome.lab.name,
+        )
+
+
+class TrackAdmin(admin.ModelAdmin):
+    list_display = ["name", "_genome_name", "_lab_name", "datetime_created"]
+
+    @admin.display(ordering="genome__name")
+    def _genome_name(self, track):
+        return format_html(
+            '<a href="/admin/tracks/genome/{}/">{}</a>',
+            track.genome.id,
+            track.genome.name,
+        )
+
+    @admin.display(ordering="lab__name")
+    def _lab_name(self, track):
+        return format_html(
+            '<a href="/admin/tracks/lab/{}/">{}</a>',
+            track.lab.id,
+            track.lab.name,
+        )
+
+
 admin.site.register(Lab)
-admin.site.register(Track)
+admin.site.register(Genome, GenomeAdmin)
+admin.site.register(Track, TrackAdmin)
