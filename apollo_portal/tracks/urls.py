61c81b2f4d6eb40ce8e5e6022755f4185ddbf0e1
Cameron Hyde <chyde@neoformit.com>
2023-12-13 16:23:19 +1000

    [broken] remove old tracks app

--- apollo_portal/tracks/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import include, path
-from . import views
-
-urlpatterns = [
-    path('', views.genomes, name='tracks_genomes'),
-    path('tracks/<int:genome_id>/', views.tracks, name='tracks_tracks'),
-    path('api/', include('tracks.api.urls')),
-]
