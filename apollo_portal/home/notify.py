f0c00acc72702ae6ea572451b41c09bca4e66bdb
Cameron Hyde <chyde@neoformit.com>
2023-12-22 09:18:42 +1000

    Add traceback to mail retry handler error message

--- apollo_portal/home/notify.py
+++ apollo_portal/home/notify.py
@@ -2,6 +2,7 @@
 
 import logging
 import time
+import traceback
 from datetime import date
 from django.conf import settings
 from django.core.mail import EmailMultiAlternatives
@@ -72,9 +73,9 @@ def send_email(form, subject, to_addresses, template):
     msg.attach_alternative(html, "text/html")
     try:
         retry_send(msg)
-    except Exception as exc:
+    except Exception:
         logger.error(
-            f'Error sending email:\n{exc}\n\n'
+            f'Error sending email:\n{traceback.format_exc()}\n\n'
             f'Submitted form values:\n{pformat(form.cleaned_data)}')
 
 
@@ -85,9 +86,8 @@ def retry_send(msg, retries=3):
                 return postal.send_mail(msg)
             return msg.send()
         except Exception as exc:
-            # if attempt == retries:
-            #     raise exc
-            raise exc
+            if attempt == retries:
+                raise exc
             logging.warning(
                 f"Email attempt {attempt} failed."
                 " Retrying in 1 second...")
