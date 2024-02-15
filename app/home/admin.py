5551593daeaed47450b2ea8bd7ac040d6e5314cb
Cameron Hyde <chyde@neoformit.com>
2024-02-15 18:12:46 +0700

    Rotating notices beneath navbar

--- app/home/admin.py
+++ app/home/admin.py
@@ -1,5 +1,37 @@
 from django.contrib import admin
 
-from .models import User
+from .admin_forms import NoticeAdminForm
+from .models import User, Notice
 
+
+class NoticeAdmin(admin.ModelAdmin):
+    """Administer notices."""
+
+    form = NoticeAdminForm
+
+    class Media:
+        """Assets for the admin page."""
+
+        js = (
+            '//cdn.jsdelivr.net/simplemde/latest/simplemde.min.js',
+            'home/js/admin-required-fields.js',
+            'home/js/admin-mde.js',
+        )
+        css = {
+            'screen': (
+                '//cdn.jsdelivr.net/simplemde/latest/simplemde.min.css',
+                'home/css/admin-mde.css',
+            ),
+        }
+
+    list_display = [
+        'datetime_modified',
+        '__str__',
+        'enabled',
+        'is_published',
+    ]
+    order = ('-datetime_modified',)
+
+
+admin.site.register(Notice, NoticeAdmin)
 admin.site.register(User)
