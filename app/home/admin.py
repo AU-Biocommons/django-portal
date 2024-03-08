17b7cd312d42179a4c3299ecbf18d016c64fdbea
Cameron Hyde <chyde@neoformit.com>
2024-03-08 15:43:35 +1000

    Fix user account passwords/forms

--- app/home/admin.py
+++ app/home/admin.py
@@ -1,5 +1,56 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
+from .admin_forms import UserCreationForm, UserChangeForm
 from .models import User
 
-admin.site.register(User)
+
+class UserAdmin(BaseUserAdmin):
+    """Administer users."""
+
+    form = UserChangeForm
+    add_form = UserCreationForm
+
+    # Remove username field
+    ordering = [
+        'first_name',
+        'last_name',
+        'email',
+        'password',
+        'is_superuser',
+        'is_staff',
+        'is_active',
+        'date_joined',
+    ]
+    list_display = [
+        'first_name',
+        'last_name',
+        'email',
+    ]
+    list_filter = ['is_staff']
+    fieldsets = (
+        (None, {'fields': ('email', 'password1', 'password2')}),
+        ('Personal', {'fields': ('first_name', 'last_name')}),
+        ('Permissions', {'fields': (
+                'is_staff',
+                'is_superuser',
+                'is_active',
+                'user_permissions',
+        )}),
+    )
+    add_fieldsets = (
+        (None, {'fields': ('email', 'password1', 'password2')}),
+        ('Personal', {'fields': ('first_name', 'last_name')}),
+        ('Permissions', {'fields': (
+                'is_staff',
+                'is_superuser',
+                'is_active',
+                'user_permissions',
+        )}),
+    )
+    search_fields = ('email',)
+    ordering = ('email',)
+    filter_horizontal = ()
+
+
+admin.site.register(User, UserAdmin)
---
+++ app/home/admin_forms.py
@@ -0,0 +1,82 @@
+"""Forms for managing generic content."""
+
+from django import forms
+from django.contrib.auth.forms import ReadOnlyPasswordHashField
+from django.core.exceptions import ValidationError
+
+from .models import User
+
+
+class UserCreationForm(forms.ModelForm):
+    """Create new user.
+
+    Includes all the required fields, plus a repeated password.
+    """
+
+    password1 = forms.CharField(label='Password', widget=forms.PasswordInput)
+    password2 = forms.CharField(
+        label='Password confirmation',
+        widget=forms.PasswordInput
+    )
+
+    class Meta:
+        model = User
+        fields = ('email', 'first_name', 'last_name')
+
+    def clean_password2(self):
+        # Check that the two password entries match
+        password1 = self.cleaned_data.get("password1")
+        password2 = self.cleaned_data.get("password2")
+        if password1 and password2 and password1 != password2:
+            raise ValidationError("Passwords don't match")
+        return password2
+
+    def save(self, commit=True):
+        # Save the provided password in hashed format
+        user = super().save(commit=False)
+        user.set_password(self.cleaned_data["password1"])
+        if commit:
+            user.save()
+        return user
+
+
+class UserChangeForm(forms.ModelForm):
+    """Update user.
+
+    Includes all the fields on
+    the user, but replaces the password field with admin's
+    disabled password hash display field.
+    """
+
+    password = ReadOnlyPasswordHashField()
+    password1 = forms.CharField(
+        label='Update Password',
+        widget=forms.PasswordInput,
+        required=False,
+    )
+    password2 = forms.CharField(
+        label='Password confirmation',
+        widget=forms.PasswordInput,
+        required=False,
+    )
+
+    class Meta:
+        model = User
+        fields = ('email', 'first_name', 'last_name')
+
+    def clean_password2(self):
+        # Check that the two password entries match
+        password1 = self.cleaned_data.get("password1")
+        password2 = self.cleaned_data.get("password2")
+        if password1 and password2 and password1 != password2:
+            raise ValidationError("Passwords don't match")
+        return password2
+
+    def save(self, commit=True):
+        # Save the provided password in hashed format
+        user = super().save(commit=False)
+        if self.cleaned_data["password1"]:
+            user.set_password(self.cleaned_data["password1"])
+        if commit:
+            user.save()
+        return user
